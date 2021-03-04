64e01d7fe4a7dc3930e4dcc5910e63c876d15776
Tobias Schweizer <t.schweizer@unibas.ch>
2021-03-04 15:53:07 +0100

    test(cypress): try to use custom commands

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -3,15 +3,15 @@ describe('Login', () => {
     it('login as a known user', () => {
         cy.visit('/');
 
-        // cy.login('anything.user01@example.org', 'test');
+        cy.login('anything.user01@example.org', 'test');
 
-        cy.get('#dologin').click();
+        /*cy.get('#dologin').click();
         cy.get('#user_id').type('anything.user01@example.org');
         cy.get('#password').type('test');
         cy.get('#login_button').click();
         cy.get('#userctrl').should(($userInfo: JQuery<HTMLElement>) => {
             expect($userInfo.get(0).innerText).to.contain('User : ');
-        });
+        });*/
     });
 
     it('logout', () => {
