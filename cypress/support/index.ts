1bab8162ec6dc636de7b086423d077ff59da103c
Tobias Schweizer <t.schweizer@unibas.ch>
2021-03-04 14:58:52 +0100

    test(cypress): change setup for Bazel

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -16,6 +16,4 @@
  *  You should have received a copy of the GNU Affero General Public
  *  License along with Knora.  If not, see <http://www.gnu.org/licenses/>.
  */
-//import * as express from "express";
-
 import "./commands";
