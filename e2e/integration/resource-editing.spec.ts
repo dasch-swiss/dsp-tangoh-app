7901e88d273736f2cd7957841f2ada58c37b8dba
Tobias Schweizer <t.schweizer@unibas.ch>
2021-06-16 11:03:34 +0200

    test(res editing): fix spec

--- e2e/integration/resource-editing.spec.ts
+++ e2e/integration/resource-editing.spec.ts
@@ -18,17 +18,17 @@ describe('Resource Editing', () => {
 
         cy.get('.result_row').first().click()
 
-        cy.get('#1 .value_container').eq(7).should('have.text', 'test')
+        cy.get('#1 .value_container').eq(9).should('have.text', 'test')
 
-        cy.get('#1 .value_container').eq(7)
+        cy.get('#1 .value_container').eq(9)
             .find('img[src="http://0.0.0.0:3335//app/icons/16x16/edit.png"]').click()
 
-        cy.get('#1 .value_container').eq(7).find('input.propedit').clear().type('testtext')
+        cy.get('#1 .value_container').eq(9).find('input.propedit').clear().type('testtext')
 
-        cy.get('#1 .value_container').eq(7)
+        cy.get('#1 .value_container').eq(9)
             .find('img[src="http://0.0.0.0:3335//app/icons/16x16/save.png"]').click()
 
-        cy.get('#1 .value_container').eq(7).should('have.text', 'testtext')
+        cy.get('#1 .value_container').eq(9).should('have.text', 'testtext')
 
         cy.get('#dologout').click();
         cy.get('#logout_button').click();
